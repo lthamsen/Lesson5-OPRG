58dc9516db82026224b5b7aa87d075dafba204c2
Lasse Thamsen <au671524@uni.au.dk>
2020-09-29 11:14:17 +0200

    Fninished ex1 l5

--- Lesson5/5.1.cpp
+++ Lesson5/5.1.cpp
@@ -6,16 +6,29 @@
 
 #define SIZE 20
 
+int A = -50;
+int B = 50;
+
 int main(void)
 {
 
 	srand(time(0));
 
 	int array1[SIZE] = {0};
+	int antalNegativeTal = 0;
 
 	for (size_t index = 0; index < SIZE; index++)
-		array1[index] = rand() %(50-(-50)+1)-50;
+		array1[index] = rand() %(B-(A)+1)+(A);
 
-	for (size_t index = 0; index < SIZE; index++)		
+	for (size_t index = 0; index < SIZE; index++)
+	{
 		printf_s("%d\n", array1[index]);
-}
+
+		if (array1[index] < 0)
+		{
+			antalNegativeTal += 1;
+		}
+			
+	}
+	printf_s("\n Der er %d negative tal.", antalNegativeTal);
+}	
